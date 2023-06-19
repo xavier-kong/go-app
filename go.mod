cc44ec3dbe92f22d96c794c741b40f66dd69de03
xavier-kong <kongxianchengxavier@gmail.com>
2023-06-19 22:36:36 +0800

    initial set up with go app and air for hmr

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/xavier-kong/go-app
+
+go 1.18
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/maxence-charriere/go-app/v9 v9.6.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"os/signal"
+	"time"
+
+	"github.com/maxence-charriere/go-app/v9/pkg/app"
+)
+
+// hello is a component that displays a simple "Hello World!". A component is a
+// customizable, independent, and reusable UI element. It is created by
+// embedding app.Compo into a struct.
+type hello struct {
+	app.Compo
+}
+
+// The Render method is where the component appearance is defined. Here, a
+// "Hello World!" is displayed as a heading.
+func (h *hello) Render() app.UI {
+	return app.H1().Text("Hello World")
+}
+
+// The main function is the entry point where the app is configured and started.
+// It is executed in 2 different environments: A client (the web browser) and a
+// server.
+func main() {
+	// The first thing to do is to associate the hello component with a path.
+	//
+	// This is done by calling the Route() function,  which tells go-app what
+	// component to display for a given path, on both client and server-side.
+	app.Route("/", &hello{})
+
+	// Once the routes set up, the next thing to do is to either launch the app or the server that serves the app.
+	//
+	// When executed on the client-side, the RunWhenOnBrowser() function
+	// launches the app,  starting a loop that listens for app events and
+	// executes client instructions. Since it is a blocking call, the code below
+	// it will never be executed.
+	//
+	// When executed on the server-side, RunWhenOnBrowser() does nothing, which
+	// lets room for server implementation without the need for precompiling
+	// instructions.
+	app.RunWhenOnBrowser()
+
+	// Finally, launching the server that serves the app is done by using the Go
+	// standard HTTP package.
+	//
+	// The Handler is an HTTP handler that serves the client and all its
+	// required resources to make it work into a web browser. Here it is
+	// configured to handle requests with a path that starts with "/".
+
+	http.Handle("/", &app.Handler{
+		Name:        "Hello",
+		Description: "An Hello World! example",
+	})
+
+	host := "127.0.0.1"
+	port := "8000"
+	ln, err := net.Listen("tcp", host+":"+port)
+	if err != nil {
+		cmd := exec.Command("fuser", "-k", "-2", "8000/tcp")
+		fmt.Println("wait")
+		err = cmd.Run()
+		if err != nil {
+			fmt.Println("died")
+		} else {
+			runServer(host, port)
+		}
+	} else {
+		ln.Close()
+		runServer(host, port)
+	}
+}
+
+func runServer(host, port string) {
+	server := http.Server{
+		Addr: host + ":" + port,
+	}
+	http.Handle("/www/", http.StripPrefix("/www/", http.FileServer(http.Dir("./www"))))
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+		}
+	}()
+
+	// Setting up signal capturing
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	// Waiting for SIGINT (kill -2)
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		// handle err
+	}
+}
